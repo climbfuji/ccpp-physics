2ff23eef515c4eaba110c715f692dd6d232e8328
Dom Heinzeller <climbfuji@ymail.com>
2021-04-07 14:24:35 -0600

    Bugfix for precipitation quantities, fix Fortran/metadata inconsistencies, use assumed-size arrays

--- physics/mp_thompson.F90
+++ physics/mp_thompson.F90
@@ -372,12 +372,12 @@ module mp_thompson
          real(kind_phys),           intent(in   ) :: dtp
          integer,                   intent(in   ) :: istep, nsteps
          ! Precip/rain/snow/graupel fall amounts and fraction of frozen precip
-         real(kind_phys),           intent(  out) :: prcp(1:ncol)
-         real(kind_phys),           intent(  out) :: rain(1:ncol)
-         real(kind_phys),           intent(  out) :: graupel(1:ncol)
-         real(kind_phys),           intent(  out) :: ice(1:ncol)
-         real(kind_phys),           intent(  out) :: snow(1:ncol)
-         real(kind_phys),           intent(  out) :: sr(1:ncol)
+         real(kind_phys),           intent(inout) :: prcp(:)
+         real(kind_phys),           intent(inout) :: rain(:)
+         real(kind_phys),           intent(inout) :: graupel(:)
+         real(kind_phys),           intent(inout) :: ice(:)
+         real(kind_phys),           intent(inout) :: snow(:)
+         real(kind_phys),           intent(  out) :: sr(:)
          ! Radar reflectivity
          real(kind_phys),           intent(  out) :: refl_10cm(1:ncol,1:nlev)
          logical,         optional, intent(in   ) :: do_radar_ref
@@ -635,11 +635,11 @@ module mp_thompson
 
          !> - Convert rainfall deltas from mm to m (on physics timestep); add to inout variables
          ! "rain" in Thompson MP refers to precipitation (total of liquid rainfall+snow+graupel+ice)
-         prcp    = max(0.0, delta_rain_mp/1000.0_kind_phys)
-         graupel = max(0.0, delta_graupel_mp/1000.0_kind_phys)
-         ice     = max(0.0, delta_ice_mp/1000.0_kind_phys)
-         snow    = max(0.0, delta_snow_mp/1000.0_kind_phys)
-         rain    = max(0.0, (delta_rain_mp - (delta_graupel_mp + delta_ice_mp + delta_snow_mp))/1000.0_kind_phys)
+         prcp    = max(0.0, prcp    + delta_rain_mp/1000.0_kind_phys)
+         graupel = max(0.0, graupel + delta_graupel_mp/1000.0_kind_phys)
+         ice     = max(0.0, ice     + delta_ice_mp/1000.0_kind_phys)
+         snow    = max(0.0, snow    + delta_snow_mp/1000.0_kind_phys)
+         rain    = max(0.0, rain    + (delta_rain_mp - (delta_graupel_mp + delta_ice_mp + delta_snow_mp))/1000.0_kind_phys)
 
          ! Recompute sr at last subcycling step
          if (nsteps>1 .and. istep == nsteps) then
